a75fc15e507b48b0deb50dec1ca581eb1718945f
prajadobe-svg <prajadobe@gmail.com>
2026-06-24 20:29:15 +0530

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,7 +9,8 @@ const nav = [
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 border-b border-slate-200 bg-white/90 backdrop-blur">
-        <script src="https://assets.adobedtm.com/e9875dd51dbe/388b9dd9eb82/launch-0197eededa87-development.min.js" async></script>
+      <script src="https://assets.adobedtm.com/6a203c8a0ff8/aeb100c194c7/launch-cce179acef2d-development.min.js" async></script>
+        {/* <script src="https://assets.adobedtm.com/e9875dd51dbe/388b9dd9eb82/launch-0197eededa87-development.min.js" async></script> */}
       <div className="container-page flex h-16 items-center justify-between">
         <Link href="/" className="text-lg font-bold">
           AdobeStore
