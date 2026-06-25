7b6e4eb4470d3a019a844f1d7f7b2a4647932583
prajadobe-svg <prajadobe@gmail.com>
2026-06-25 13:53:02 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+              <script src="https://assets.adobedtm.com/6a203c8a0ff8/aeb100c194c7/launch-59192a116ea8.min.js" async></script>
         <SiteHeader />
+        
+        
         <main>{children}</main>
         <SiteFooter />
       </body>
