80c64d711b3184ae03df228f1ecde422623a76f7
Prajwal Singh <prajsingh@adobe.com>
2026-08-20 16:07:36 +0530

    back to user id

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
-export const DEMO_USER_ID = "praj-store-demo-user-1002";
-export const DEMO_USER_EMAIL = "demo1.user@company.com";
+export const DEMO_USER_ID = "praj-store-demo-user-1001";
+export const DEMO_USER_EMAIL = "demo.user@company.com";
 export const DEMO_USER_PASSWORD = "demo123";
 export const AUTH_TOKEN_KEY = "authToken";
 
